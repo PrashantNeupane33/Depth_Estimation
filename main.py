3367a5bdb1c69c3578236f6a04d44776d64f737d
PrashantNeupane33 <www.panbr123@gmail.com>
2024-12-29 12:44:41 +0545

    Testing OpenCV tracker

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import cv2
 import depthai as dai
 from calc import HostSpatialsCalc
-from utility import *
+from utility import recMask, rescale
 import numpy as np
 import time
 from ultralytics import YOLO
 
 conf_threshold = 0.75
 model_path = "best.pt"
+detect = True
+count = 0
 
 
 # Create pipeline
@@ -52,13 +54,13 @@ xoutRgb = pipeline.create(dai.node.XLinkOut)
 xoutRgb.setStreamName("rgb")
 camRgb.preview.link(xoutRgb.input)
 
-file = open(r"data.csv", "a")
-file.write("-----------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
-file.write("time, x, y, z\n")
+# file = open(r"data.csv", "a")
+# file.write("-----------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
+# file.write("time, x, y, z\n")
 
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
-    # Output queue will be used to get the depth frames from the outputs defined above
+
     depthQueue = device.getOutputQueue(name="depth")
     dispQ = device.getOutputQueue(name="disp")
     rgbQueue = device.getOutputQueue("rgb", 4, False)
@@ -81,40 +83,58 @@ with dai.Device(pipeline) as device:
 
         rgbFrame = rgbData.getCvFrame()
         # rgbFrame = cv2.GaussianBlur(rgbFrame, (7,7), 0)
-        results = model.track(source=rgbFrame, show=False, tracker="new.yaml")
-
-        for r in results:
-            cv2.imshow("Balls", rgbFrame)
-            if r.boxes is None:
-                continue
-
-            for box in r.boxes:
-                if float(box.conf) < conf_threshold:
-                    continue
-
-                bx = box.cpu().xywh.numpy()
-                imgH, imgW = r.orig_shape
-
-                if bx.shape[0] < 0:
-                    continue
-
-                x, y, w, h = bx[0]
-                cv2.rectangle(rgbFrame, (int(x-w/2), int(y-h/2)),
-                              (int(x+w/2), int(y+h/2)), (0, 255, 0), 2)
+        if detect:
+            results = model.predict(rgbFrame)
+            for r in results:
                 cv2.imshow("Balls", rgbFrame)
+                if r.boxes is None:
+                    continue
 
+                for box in r.boxes:
+                    if float(box.conf) < conf_threshold:
+                        continue
+
+                    bx = box.cpu().xywh.numpy()
+                    imgH, imgW = r.orig_shape
+
+                    if bx.shape[0] < 0:
+                        continue
+                    bbox = bx[0]
+                    x, y, w, h = bbox
+                    cv2.rectangle(rgbFrame, (int(x-w/2), int(y-h/2)),
+                                  (int(x+w/2), int(y+h/2)), (0, 255, 0), 2)
+                    detect = False
+
+                    # Initialize Tracker
+                    tracker = cv2.TrackerCSRT_create()
+                    bbox_tracker = (
+                        int(x - w / 2), int(y - h / 2), int(w), int(h))
+                    tracker.init(rgbFrame, bbox_tracker)
+                    break
+        else:
+            cv2.imshow("Balls", rgbFrame)
+            if tracker:
+                success, bbox = tracker.update(rgbFrame)
+                if success:
+                    x, y, w, h = [int(v) for v in bbox]
+                    cv2.rectangle(rgbFrame, (x, y),
+                                  (x + w, y + h), (0, 255, 0), 2)
+                    count += 1
+                    if count > 100:
+                        detect = True
+                        count = 0
         # Calculate spatial coordiantes from depth frame
         spatials, centroid = hostSpatials.calc_spatials(
             depthData, (int(x), int(y)))
-        file.write(str(time.time() - prev_time))
-        prev_time = time.time()
-        file.write(', ')
-        file.write(str(spatials['x']))
-        file.write(', ')
-        file.write(str(spatials['y']))
-        file.write(', ')
-        file.write(str(spatials['z']))
-        file.write('\n')
+        # file.write(str(time.time() - prev_time))
+        # prev_time = time.time()
+        # file.write(', ')
+        # file.write(str(spatials['x']))
+        # file.write(', ')
+        # file.write(str(spatials['y']))
+        # file.write(', ')
+        # file.write(str(spatials['z']))
+        # file.write('\n')
 
         # Get disparity frame for nicer depth visualization
         disp = dispQ.get().getFrame()
